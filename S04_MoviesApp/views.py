ff6d3e4346a09c12ef2ea19465299fa8d140414d
Tibor Hajszan <thajszan@t-online.hu>
2024-11-08 17:31:01 +0100

    Lesson "My Fav Movies Template" : Completed.

--- S04_MoviesApp/views.py
+++ S04_MoviesApp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def movies_index(request):
-    return HttpResponse("My Favorite Movies")
+    return render(request, "S04_MoviesApp/index.html", {})
