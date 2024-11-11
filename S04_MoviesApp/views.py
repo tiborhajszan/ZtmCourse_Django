319023a2ac5909aa6a65384481afe6eb5db6749a
Tibor Hajszan <thajszan@t-online.hu>
2024-11-11 16:01:07 +0100

    Lesson "Accessing Variables in Django Templates" : Completed.

--- S04_MoviesApp/views.py
+++ S04_MoviesApp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def movies_index(request):
-    return render(request, "S04_MoviesApp/index.html", {})
+    return render(request, "S04_MoviesApp/index.html", {"movie":"Star Wars"})
 
 def movies_about(request):
     return render(request, "S04_MoviesApp/about.html", {})
