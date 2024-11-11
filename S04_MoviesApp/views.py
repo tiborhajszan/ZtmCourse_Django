e831a034d22e17ca62554d4153a5cb421d6319fd
Tibor Hajszan <thajszan@t-online.hu>
2024-11-11 16:31:30 +0100

    Lesson "Listing Out Our Favorite Movies" : Completed.

--- S04_MoviesApp/views.py
+++ S04_MoviesApp/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def movies_index(request):
-    return render(request, "S04_MoviesApp/index.html", {"movie":"Star Wars"})
+    context = {"movies": ["star wars", "harry potter", "lord of the rings", "star trek", "the mission"]}
+    return render(request, "S04_MoviesApp/index.html", context)
 
 def movies_about(request):
     return render(request, "S04_MoviesApp/about.html", {})
