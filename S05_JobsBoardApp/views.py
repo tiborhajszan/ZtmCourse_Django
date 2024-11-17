60fee0718e42daac2ff0fee400911de895ae9bad
Tibor Hajszan <thajszan@t-online.hu>
2024-11-17 14:17:36 +0100

    Lesson "Listing Out Active Jobs in Our Template" : Completed.

--- S05_JobsBoardApp/views.py
+++ S05_JobsBoardApp/views.py
@@ -3,5 +3,6 @@ from .models import JobPosting
 
 # Create your views here.
 def jobs_index(request):
-    active_jobs = JobPosting.objects.filter(active=True)
-    return HttpResponse("<h1>Jobs Board</h1>")
+    active_jobs = JobPosting.objects.filter(is_active=True)
+    context = {"active_jobs": active_jobs}
+    return render(request, "S05_JobsBoardApp/index.html", context)
