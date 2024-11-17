50046e5451c70b55147ce607fa0c0e76e71d44b8
Tibor Hajszan <thajszan@t-online.hu>
2024-11-17 13:27:06 +0100

    Lesson "Adding New Field to Job Posting Table" : Completed.

--- S05_JobsBoardApp/models.py
+++ S05_JobsBoardApp/models.py
@@ -6,3 +6,4 @@ class JobPosting(models.Model):
     description = models.TextField()
     company = models.CharField(max_length=100)
     salary = models.IntegerField()
+    is_active = models.BooleanField(default=False)
