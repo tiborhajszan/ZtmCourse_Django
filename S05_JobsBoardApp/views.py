6b91f6b967498694fbd3ac9c472a16b929ad6d25
Tibor Hajszan <thajszan@t-online.hu>
2024-11-17 13:44:37 +0100

    Lesson "Grabbing the Active Job Posting in Our View" : Completed.

--- S05_JobsBoardApp/views.py
+++ S05_JobsBoardApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from .models import JobPosting
 
 # Create your views here.
 def jobs_index(request):
+    active_jobs = JobPosting.objects.filter(active=True)
     return HttpResponse("<h1>Jobs Board</h1>")
