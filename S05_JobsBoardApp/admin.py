eeb3c5f4ca005b77db4943937bd4beba6fa4297b
Tibor Hajszan <thajszan@t-online.hu>
2024-11-20 12:41:37 +0100

    Lesson "Registering the Job Board Model in the Admin" : Completed.

--- S05_JobsBoardApp/admin.py
+++ S05_JobsBoardApp/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+#####################################################################################
+### Django Bootcamp - Section 06 - Jobs Board Application - Models Admin Register ###
+#####################################################################################
 
-# Register your models here.
+### imports
+from django.contrib import admin
+from .models import JobPosting
+
+### registering job posting model
+admin.site.register(JobPosting)
