7e8a63bbb13a8e968c7589dfce7466847c10b8bb
Tibor Hajszan <thajszan@t-online.hu>
2024-11-20 13:30:25 +0100

    Lesson "Making the Job Postings Appear Better in the Admin Section" : Completed.

--- S05_JobsBoardApp/models.py
+++ S05_JobsBoardApp/models.py
@@ -1,8 +1,14 @@
+########################################################################################################################
+### Django Bootcamp :: Section 05/06
+### Jobs Board Application :: Model Class Definitions
+########################################################################################################################
 
 ### imports
 from django.db import models
 
+########################################################################################################################
 ### job posting model class
+########################################################################################################################
 class JobPosting(models.Model):
     """
     Interface to the Job Posting database table.
@@ -15,9 +21,19 @@ class JobPosting(models.Model):
     - is_active: bool, True = active posting | False = inactive posting
     """
 
-    ### attributes: job posting table fields (columns)
+    ### attributes: job posting table fields (columns) #################################################################
     title = models.CharField(max_length=100)
     description = models.TextField()
     company = models.CharField(max_length=100)
     salary = models.IntegerField()
     is_active = models.BooleanField(default=False)
+
+    ### str dunder method override #####################################################################################
+    def __str__(self):
+        """
+        Overrides dunder method for str object representation.
+
+        Returns:
+        - str, <id> | <title> | <company> | <Active/Inactive>
+        """
+        return f"{self.id}: {self.title} | {self.company} | {'Active' if self.is_active else 'Inactive'}"
