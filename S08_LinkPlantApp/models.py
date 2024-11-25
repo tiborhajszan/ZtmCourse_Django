4c3487ad70feb0e913f0659592022672c0715fad
Tibor Hajszan <thajszan@t-online.hu>
2024-11-25 13:52:30 +0100

    Lesson "Setting Up the Profile and Link Models" : Completed.

--- S08_LinkPlantApp/models.py
+++ S08_LinkPlantApp/models.py
@@ -1,3 +1,22 @@
+
 from django.db import models
 
 # Create your models here.
+class ProfileModel(models.Model):
+
+    BG_CHOICES = (
+        ("blue", "Blue"),
+        ("green", "Green"),
+        ("yellow", "Yellow"),)
+    
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    bg_color = models.CharField(max_length=50, choices=BG_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+class LinkModel08(models.Model):
+    text = models.CharField(max_length=100)
+    url = models.URLField()
+    profile = models.ForeignKey(to=ProfileModel, on_delete=models.CASCADE, related_name='links')
